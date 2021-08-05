e1947bf145093a9fc446907730a88ae8cf6b2c1e
kcwong232485 <kcwong232485@hotmail.com>
2021-08-06 01:36:30 +0800

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,16 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Ratingstar />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+//import HelloWorld from './components/HelloWorld.vue'
+import Ratingstar from './components/Ratingstar.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Ratingstar,
   }
 }
 </script>
